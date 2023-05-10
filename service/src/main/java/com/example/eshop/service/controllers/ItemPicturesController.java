1aba913ad85556e4630e672a7ea010e569277446
bayati <qasim.al3ra8e@gmail.com>
2023-05-10 17:15:55 +0300

    chore: fix file downloading behavior.

--- service/src/main/java/com/example/eshop/service/controllers/ItemPicturesController.java
+++ service/src/main/java/com/example/eshop/service/controllers/ItemPicturesController.java
@@ -5,9 +5,13 @@ import com.example.contract.repositories.ItemsRepository;
 import com.example.eshop.service.controllers.resources.DocumentResponseResource;
 import com.example.eshop.service.exceptions.ResourceNotFoundException;
 import com.example.eshop.service.resourcemappers.ItemResourceMapper;
+import com.example.modals.Document;
 import com.example.modals.Item;
 import org.springframework.hateoas.Link;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,29 +46,36 @@ public class ItemPicturesController {
                 .collect(Collectors.toList());
     }
 
+    @GetMapping(value = "/{reference}/download")
+    public ResponseEntity<byte[]> downloadPicture(@PathVariable Long itemId,
+                                                  @PathVariable String reference) {
+        final Document documentItemDocument = getDocumentItemDocument(itemId, reference);
+        byte[] content = documentItemDocument.getContent();
+        final HttpHeaders headers = new HttpHeaders();
 
+        headers.setContentType(MediaType.valueOf(
+                documentItemDocument.getContentType()));
 
-    @GetMapping(value = "/{reference}/download", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-    public byte[] downloadPicture(@PathVariable Long itemId,
-                                  @PathVariable String reference) {
-        return getDocumentContent(itemId, reference);
+        return new ResponseEntity<>(content, headers, HttpStatus.OK);
     }
 
     @GetMapping(value = "/{reference}/download", produces = MediaType.APPLICATION_JSON_VALUE)
     public Map<String, String> downloadPictureEncoded(@PathVariable Long itemId,
                                                       @PathVariable String reference) {
-        byte[] content = getDocumentContent(itemId, reference);
+        Document document = getDocumentItemDocument(itemId, reference);
+        byte[] content = document.getContent();
 
-        return Map.of("content", new String(Base64.getEncoder().encode(content)));
+        final String base64Prefix = String.format("data:%s;base64,", document.getContentType());
+
+        return Map.of("content", base64Prefix.concat(Base64.getEncoder().encodeToString(content)));
     }
 
-    private byte[] getDocumentContent(Long itemId, String reference) {
+    private Document getDocumentItemDocument(Long itemId, String reference) {
         return itemsRepository.findById(itemId)
                 .getPictures()
                 .stream()
                 .filter(it -> Objects.equals(it.getReference(), reference)).findFirst()
-                .orElseThrow(ResourceNotFoundException::new)
-                .getContent();
+                .orElseThrow(ResourceNotFoundException::new);
     }
 
     private void addDownloadLink(Long itemId, DocumentResponseResource documentResource) {
